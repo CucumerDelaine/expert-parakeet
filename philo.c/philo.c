38cec5e46956c4ed6641727314e5099739e47097
Cucumber Delaine <cdelaine@ge-c4.kzn.21-school.ru>
2021-12-06 19:54:09 +0300

    1234

--- philo.c/philo.c
+++ philo.c/philo.c
@@ -3,13 +3,95 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdelaine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cdelaine <cdelaine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 13:24:54 by cdelaine          #+#    #+#             */
-/*   Updated: 2021/12/06 13:24:55 by cdelaine         ###   ########.fr       */
+/*   Updated: 2021/12/06 19:54:02 by cdelaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void *check_dead(void *philo_r)
+{
+	t_philo *philo;
+
+	philo = philo_r;
+	while(1)
+	{
+		pthread_mutex_lock(&philo->action);
+		if (!(philo->eat_now) && get_time() > philo->time_limit)
+		{
+			print_message(5, philo);
+			pthread_mutex_unlock(&philo->action);
+			pthread_mutex_unlock(&philo->info->check_dead);
+			return ((void *)0);
+		}
+		pthread_mutex_unlock(&philo->action);
+		usleep(1000);
+	}
+}
+
+void *life(void *philo_r)
+{
+	t_philo *philo;
+	pthread_t thread_id;
+
+	philo = philo_r;
+	philo->eat_start = get_time();
+	philo->time_limit  = philo->eat_start + philo->info->time_to_die;
+	pthread_create(&thread_id, NULL, &check_dead, philo_r);
+	pthread_detach(thread_id);
+	while(1)
+	{
+		do_eat(philo);
+		do_sleep(philo);
+		print_message(4, philo);
+	}
+}
+
+void *how_meals(void *info_r)
+{
+	int i;
+	int eat;
+	t_info *info;
+
+	eat = 0;
+	info = info_r;
+	while (eat < info->how_much_eat)
+	{
+		i = 0;
+		while (i < info->philo_num)
+			pthread_mutex_lock(&info->philo[i++].eating);
+		eat++;
+	}
+	print_message(6, &info->philo[0]);
+	pthread_mutex_unlock(&info->check_dead);
+}
+
+int start_pthread(t_info *info)
+{
+	int i;
+	pthread_t thread_id;
+	t_philo *philo;
+
+	i = 0;
+	info->start_time = get_time();
+	if (info->how_much_eat > 0)
+	{
+		if (pthread_create(&thread_id, NULL, &how_meals, info))
+			return (1);
+		pthread_detach(thread_id);
+	}
+	while (i < info->philo_num)
+	{
+		philo = &info->philo[i];
+		if (pthread_create(&thread_id, NULL, &life, info))
+			return (1);
+		pthread_detach(thread_id);
+		usleep(100);
+		i++;
+	}
+	return (0);
+}
 
 void clean_mem(t_info *info)
 {
